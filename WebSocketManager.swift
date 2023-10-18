15e96be8811c55530b0f1fa4abf62375b2d45ddc
powskierman <mlapoint99@hotmail.com>
2023-10-18 11:19:56 -0400

    Refactored WebSocketManager

--- WebSocketManager.swift
+++ WebSocketManager.swift
@@ -1,46 +1,38 @@
 import Starscream
 import Combine
 
-
 public class WebSocketManager: ObservableObject {
     public static let shared = WebSocketManager()
-    lazy var homeAssistantWebSocket = HassWebSocket.shared
-    weak var delegate: WebSocketManagerDelegate?
-
-//public class WebSocketManager: ObservableObject {
-    // Singleton pattern: This provides a shared instance of WebSocketManager
+    
+    // No need to re-declare the `HassWebSocket` instance; we already have it declared once as 'websocket'
     public var websocket = HassWebSocket.shared
-//    public static let shared = WebSocketManager()
-
+    
     @Published public var connectionState: ConnectionState = .disconnected
     @Published public var eventsReceived: [String] = []
-    //@Published public var leftDoorClosed: Bool = true
 
-
- //   private var homeAssistantWebSocket = HassWebSocket.shared
     private var cancellables: Set<AnyCancellable> = []
 
     private init() {
-        homeAssistantWebSocket.$connectionState
+        websocket.$connectionState
             .assign(to: \.connectionState, on: self)
             .store(in: &cancellables)
     }
-    
+
     public var onConnected: (() -> Void)? {
         didSet {
-            homeAssistantWebSocket.onConnected = onConnected
+            websocket.onConnected = onConnected
         }
     }
 
     public var onDisconnected: (() -> Void)? {
         didSet {
-            homeAssistantWebSocket.onDisconnected = onDisconnected
+            websocket.onDisconnected = onDisconnected
         }
     }
 
     public var onEventReceived: ((String) -> Void)? {
         didSet {
-            homeAssistantWebSocket.onEventReceived = { [weak self] event in
+            websocket.onEventReceived = { [weak self] event in
                 DispatchQueue.main.async {
                     self?.eventsReceived.append(event)
                 }
@@ -50,32 +42,32 @@ public class WebSocketManager: ObservableObject {
     }
 
     public func connect() {
-        homeAssistantWebSocket.connect()
+        websocket.connect()
     }
     
     public func disconnect() {
-        homeAssistantWebSocket.disconnect()
+        websocket.disconnect()
     }
     
     public func subscribeToEvents() {
-        homeAssistantWebSocket.subscribeToEvents()
+        websocket.subscribeToEvents()
     }
     
     func handleEventMessage(_ message: [String: Any]) {
         guard let eventType = message["event_type"] as? String,
               eventType == "state_changed",
               let data = message["data"] as? [String: Any],
-              let entityId = data["entity_id"] as? String,
-              let newStateData = data["new_state"] as? [String: Any],
-              let newState = newStateData["state"] as? String else {
+              // If you don't use these variables, either replace them with _ or comment them out
+              let _ = data["entity_id"] as? String,
+              let _ = (data["new_state"] as? [String: Any])?["state"] as? String
+        else {
             return
         }
-
-//        if entityId == "switch.left_door" {
-//            self.leftDoorClosed = (newState == "off")
-//        }
-        // Handle other entities similarly
+        // If you plan to use entityId and newState later, you can uncomment the below lines
+        // let entityId = data["entity_id"] as! String
+        // let newState = (data["new_state"] as! [String: Any])["state"] as! String
     }
+    
     func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
         guard let data = text.data(using: .utf8),
               let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
@@ -86,7 +78,5 @@ public class WebSocketManager: ObservableObject {
         if let messageType = message["type"] as? String, messageType == "event" {
             handleEventMessage(message)
         }
-        // Handle other message types...
     }
-
 }
