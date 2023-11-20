b45604be5c983c4217815650d11436e1fd4e2e1e
powskierman <mlapoint99@hotmail.com>
2023-11-19 23:11:17 -0700

    Reset to working. added new xcframework without watch

---
+++ WatchFramework/HassFramework.h
@@ -0,0 +1,18 @@
+//
+//  WatchFramework.h
+//  WatchFramework
+//
+//  Created by Michel Lapointe on 2023-11-18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WatchFramework.
+FOUNDATION_EXPORT double WatchFrameworkVersionNumber;
+
+//! Project version string for WatchFramework.
+FOUNDATION_EXPORT const unsigned char WatchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WatchFramework/PublicHeader.h>
+
+
